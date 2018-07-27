1d5753e9c2a8b739b91bb7e649e933ea090ccdba
Unknown <v.nikanorov.upwork@gmail.com>
2018-07-27 10:16:19 +0300

    Settings for deploy app to github pages

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './header.component.css';
 
 const Header = ({ location }) => (
   <div className="header">
     <div className="country">
-      {/* {location.country_name} */}
+      <div className="header-title">
+        Wether App:
+        {location.country_name}
+      </div>
+      <div className="btn-menu">
+        <span />
+        <span />
+        <span />
+      </div>
     </div>
   </div>
 );
